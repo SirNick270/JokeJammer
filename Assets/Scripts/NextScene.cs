2c9de23a12101a044e5210d2439d54574b76cc8c
SirNick270 <nicolasfreiria27@gmail.com>
2024-01-28 01:51:21 +0100

    Ultima versión

---
+++ Assets/Scripts/NextScene.cs
@@ -0,0 +1,37 @@
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class CambiarEscena : MonoBehaviour
+{
+    public int escenaSiguiente;
+    public Points points;
+    public ProgressBar progressBar;
+    private Button bt;
+
+    private void Start()
+    {
+        bt = GetComponent<Button>();
+        bt.interactable = false;
+    }
+
+    private void Update()
+    {
+        if (points.pps >= progressBar.ppsMax)
+        {
+            bt.interactable = true;
+        }
+        else
+        {
+            bt.interactable = false;
+        }
+    }
+
+    public void NextScene()
+    {
+        // Cambia a la escena llamada "Escena2"
+        SceneManager.LoadScene("Escena" + escenaSiguiente);
+    }
+}
+
